cdd12c0511daeb2eca38e893ec037c2e619efe28
geetapuri999 <geetapuri999@gmail.com>
2019-02-01 18:42:46 +0000

    Updated Adhoc Scheduler port

--- src/main/java/com/iig/gcp/scheduler/schedulerController/utils/DataSourceConfig.java
+++ src/main/java/com/iig/gcp/scheduler/schedulerController/utils/DataSourceConfig.java
@@ -60,6 +60,7 @@ class DataSourceConfig {
 		String orcl_decoded_pwd=EncryptionUtil.decryptText(base_pwd, EncryptionUtil.decodeKeyFromString(content));
 		
 		HikariConfig jdbcConfig = new HikariConfig();
+		jdbcConfig.setLeakDetectionThreshold(10000);
         jdbcConfig.setPoolName(poolName);
         jdbcConfig.setMaximumPoolSize(maximumPoolSize);
         jdbcConfig.setMinimumIdle(minimumIdle);
